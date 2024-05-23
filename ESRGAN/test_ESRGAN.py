c5df0d3f75786c7d211a5e8d93be5e3e5bab4c15
bwj2800 <bwj2800@gmail.com>
2024-05-23 14:41:58 +0900

    edit for teseting

--- ESRGAN/test_ESRGAN.py
+++ ESRGAN/test_ESRGAN.py
@@ -9,14 +9,15 @@ from torchvision.utils import save_image
 from torcheval.metrics.functional import peak_signal_noise_ratio
 from skimage.metrics import structural_similarity as ssim
 import numpy as np
+from tqdm import tqdm
 
-MODEL_NAME='shaken_model2'
-checkpoint='generator_227'
+MODEL_NAME='all_model2'
+checkpoint=461
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset_root", type=str, default="../datasets/validation", required=False, help="Path to image")
-parser.add_argument("--dataset_txt_path", type=str, default="../datasets/new_val_shaken.txt", required=False, help="Path to image")
+parser.add_argument("--dataset_txt_path", type=str, default="../datasets/new_val_all.txt", required=False, help="Path to image")
 parser.add_argument("--output_path", type=str, default="../result/esrgan/images/test/"+MODEL_NAME, required=False, help="Path to image")
-parser.add_argument("--checkpoint_model", type=str, default="../result/esrgan/saved_model/"+MODEL_NAME+"/"+checkpoint+".pth", required=False, help="Path to checkpoint model")
+parser.add_argument("--checkpoint_model", type=str, default="../result/esrgan/saved_model/"+MODEL_NAME+"/generator_"+str(checkpoint)+".pth", required=False, help="Path to checkpoint model")
 parser.add_argument("--channels", type=int, default=3, help="Number of image channels", required=False)
 parser.add_argument("--residual_blocks", type=int, default=23, help="Number of residual blocks in G", required=False)
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation", required=False)
@@ -36,72 +37,93 @@ os.makedirs(opt.output_path, exist_ok=True)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Define model and load model checkpoint
-generator = GeneratorRRDB(opt.channels, filters=64, num_res_blocks=opt.residual_blocks).to(device)
-generator.load_state_dict(torch.load(opt.checkpoint_model))
-generator.eval()
+RESULT=""
+best_psnr=0
+best_psnr_checkpoint=0
+best_ssim=0
+best_ssim_checkpoint=0
 
-Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.Tensor
+for c in tqdm(range(1)):
+    # Define model and load model checkpoint
+    generator = GeneratorRRDB(opt.channels, filters=64, num_res_blocks=opt.residual_blocks).to(device)
+    # generator.load_state_dict(torch.load("../result/esrgan/saved_model/"+MODEL_NAME+"/generator_"+str(c)+".pth"))
+    generator.load_state_dict(torch.load("../result/esrgan/saved_model/"+MODEL_NAME+"/generator_49.pth"))
+    generator.eval()
 
-dataloader = DataLoader(
-    TestImageDataset(root=opt.dataset_root, text_file_path = opt.dataset_txt_path, shape=(opt.hr_height, opt.hr_width)),
-    batch_size=1,
-    shuffle=True,
-    num_workers=opt.n_cpu,
-)
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.Tensor
 
-psnr_val = []
-ssim_val = []
+    dataloader = DataLoader(
+        TestImageDataset(root=opt.dataset_root, text_file_path = opt.dataset_txt_path, shape=(opt.hr_height, opt.hr_width)),
+        batch_size=1,
+        shuffle=True,
+        num_workers=opt.n_cpu,
+    )
 
-# Upsample image
-with torch.no_grad():
-    for i, imgs in enumerate(dataloader):
+    psnr_val = []
+    ssim_val = []
 
-        batches_done = i
+    # Upsample image
+    with torch.no_grad():
+        for i, imgs in enumerate(dataloader):
 
-        # Configure model input
-        imgs_lr = Variable(imgs["lr"].type(Tensor))
-        imgs_hr = Variable(imgs["gt"].type(Tensor))
+            batches_done = i
 
-        # Generate a high resolution image from low resolution input
-        gen_hr = generator(imgs_lr)
+            # Configure model input
+            imgs_lr = Variable(imgs["lr"].type(Tensor))
+            imgs_hr = Variable(imgs["gt"].type(Tensor))
 
-        # calculate PSNR
-        psnr=peak_signal_noise_ratio(denormalize(imgs_hr), denormalize(gen_hr))
-        psnr_val.append(psnr)
+            # Generate a high resolution image from low resolution input
+            gen_hr = generator(imgs_lr)
 
-        # calculate SSIM
-        np_imgs_hr = tensor_to_numpy(imgs_hr)
-        np_gen_hr = tensor_to_numpy(gen_hr)
-        
-        np_imgs_hr = np.squeeze(np_imgs_hr, axis=0)
-        np_gen_hr = np.squeeze(np_gen_hr, axis=0)
+            # calculate PSNR
+            psnr=peak_signal_noise_ratio(denormalize(imgs_hr), denormalize(gen_hr))
+            psnr_val.append(psnr)
 
-        np_imgs_hr = np.transpose(np_imgs_hr, (1, 2, 0))
-        np_gen_hr = np.transpose(np_gen_hr, (1, 2, 0))
+            # calculate SSIM
+            np_imgs_hr = tensor_to_numpy(imgs_hr)
+            np_gen_hr = tensor_to_numpy(gen_hr)
+            
+            np_imgs_hr = np.squeeze(np_imgs_hr, axis=0)
+            np_gen_hr = np.squeeze(np_gen_hr, axis=0)
 
-        ssim_value = ssim(np_imgs_hr, np_gen_hr, channel_axis=2, data_range=1.0)
+            np_imgs_hr = np.transpose(np_imgs_hr, (1, 2, 0))
+            np_gen_hr = np.transpose(np_gen_hr, (1, 2, 0))
 
-        ssim_val.append(ssim_value)
+            ssim_value = ssim(np_imgs_hr, np_gen_hr, channel_axis=2, data_range=1.0)
 
-        print(
-            "[Batch %d/%d] [PSNR: %f] [SSIM: %f] %s"
-            % (
-                i,
-                len(dataloader),
-                psnr,
-                ssim_value,
-                imgs["file_name"][0]
-            )
-        )
-        # Save image
-        save_image(denormalize(gen_hr).cpu(), opt.output_path+f"/sr-"+imgs["file_name"][0])
+            ssim_val.append(ssim_value)
 
-print("Average PSNR:",(sum(psnr_val) / len(psnr_val)).item())
-print("Average SSIM:", (sum(ssim_val) / len(ssim_val)).item())
+            # print(
+            #     "[Batch %d/%d] [PSNR: %f] [SSIM: %f] %s"
+            #     % (
+            #         i,
+            #         len(dataloader),
+            #         psnr,
+            #         ssim_value,
+            #         imgs["file_name"][0]
+            #     )
+            # )
 
-RESULT = "\n{} {} Average PSNR: {:.5f} Average SSIM: {:.5f}".format(MODEL_NAME, checkpoint, sum(psnr_val) / len(psnr_val), sum(ssim_val) / len(ssim_val))
+            # Save image
+            save_image((gen_hr).cpu(), opt.output_path+f"/sr-"+imgs["file_name"][0])
 
-f=open('../result/esrgan/psnr.txt','a')
+    avg_psnr=sum(psnr_val) / len(psnr_val)
+    avg_ssim=sum(ssim_val) / len(ssim_val)
+    print("Average PSNR:", avg_psnr)
+    print("Average SSIM:", avg_ssim)
+
+    RESULT += "\n{} Average PSNR: {:.5f} Average SSIM: {:.5f}".format(c, avg_psnr, avg_ssim)
+
+    if avg_psnr>best_psnr:
+        best_psnr=avg_psnr
+        best_psnr_checkpoint=c
+    if avg_ssim>best_ssim:
+        best_ssim=avg_ssim
+        best_ssim_checkpoint=c
+
+RESULT += "\nBest Average PSNR at generator_{}: {:.5f}".format(best_psnr_checkpoint, best_psnr)
+RESULT += "\nBest Average SSIM at generator_{}: {:.5f}".format(best_ssim_checkpoint, best_ssim)
+
+f=open('../result/esrgan/'+MODEL_NAME+'.txt','a')
 f.write(RESULT)
 f.close()
